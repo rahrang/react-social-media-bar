dbed3ddf6ea36867a5add594b88b43a44ae9e46d
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-11-15 10:59:37 -0800

    passing down props correctly, styling/formatting correctly as well, need to check that margin/padding work as expected, conduct more testing

--- social-media-bar-test/src/App.js
+++ social-media-bar-test/src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 import SocialMediaBar from 'react-social-media-bar';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
@@ -17,14 +15,46 @@ class App extends Component {
         </p>
         This is our SocialMediaBar component:
         <SocialMediaBar
-          vertical
+          // vertical
           bgColor="#FFD700"
           iconColor="#895FAD"
           iconSize={'40px'}
+          icons={ICONS}
+          padding={{ top: '6px', left: '3px', right: '12px', bottom: '9px' }}
         />
       </div>
     );
   }
 }
+
+const ICONS = [
+  {
+    media: 'quora',
+    link: 'https://quora.com',
+    iconColor: '#289079'
+  },
+  {
+    media: 'facebook',
+    link: 'https://facebook.com',
+    iconColor: 'green',
+    hoverColor: 'red',
+    iconOpacity: 0.5,
+    hoverOpacity: 1,
+    sameTab: true
+  },
+  {
+    media: 'twitter',
+    link: 'https://twitter.com',
+    hoverColor: 'black',
+    iconOpacity: 0.5,
+    hoverOpacity: 1,
+    padding: {
+      top: '12px',
+      right: '14px',
+      left: '100px',
+      bottom: '1px'
+    }
+  }
+];
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,54 @@ import styled from 'styled-components';
 export default class SocialMediaBar extends React.Component {
   render() {
     console.log(this.props);
-    let { bgColor, iconColor, iconSize, vertical } = this.props;
+
+    let {
+      icons,
+      bgColor,
+      iconColor,
+      brandColors,
+      iconSize,
+      vertical,
+      hoverOpacity,
+      hoverColor,
+      sameTab
+    } = this.props;
+
+    let generatedIcons = icons.map((icon, index) => {
+      let {
+        link,
+        iconColor,
+        iconOpacity,
+        iconSize,
+        hoverOpacity,
+        hoverColor,
+        sameTab,
+        margin,
+        padding
+      } = icon;
+      return (
+        <IconAnchor
+          key={`${icon.media}_${index}`}
+          href={icon.link}
+          target={icon.sameTab ? '_self' : '_blank'}
+          iconColor={icon.iconColor || iconColor}
+          iconOpacity={icon.iconOpacity || iconOpacity}
+          iconSize={icon.iconSize || iconSize}
+          hoverOpacity={icon.hoverOpacity || hoverOpacity}
+          hoverColor={icon.hoverColor || hoverColor}
+          margin={icon.margin || margin}
+          padding={icon.padding || padding}
+        >
+          <i className={`fa-${icon.media} fa`} aria-hidden="true" />
+        </IconAnchor>
+      );
+    });
+
     return (
       <div>
         <h1>This is so SocialMsdfsaediaBar!</h1>
-        <ContainerDiv
-          bgColor={bgColor}
-          iconColor={iconColor}
-          iconSize={iconSize}
-          vertical={vertical}
-        >
-          <p>In the container div</p>
-          <h2>In the container div</h2>
-          <button>In the container div</button>
+        <ContainerDiv bgColor={bgColor} vertical={vertical}>
+          {generatedIcons}
         </ContainerDiv>
       </div>
     );
@@ -26,24 +61,100 @@ export default class SocialMediaBar extends React.Component {
 
 const ContainerDiv = styled.div`
   display: flex;
-  flex-direction: ${({ vertical }) => 'column'};
+  flex-direction: ${({ vertical }) => vertical && 'column'};
   align-items: center;
   justify-content: center;
   background-color: ${({ bgColor }) => bgColor};
-  color: ${({ iconColor }) => iconColor};
-  font-size: ${({ iconSize }) => iconSize};
 `;
 
 ContainerDiv.propTypes = {
+  icons: PropTypes.arrayOf(PropTypes.object).isRequired,
   bgColor: PropTypes.string,
   iconColor: PropTypes.string,
+  iconOpacity: PropTypes.number,
   iconSize: PropTypes.string,
-  vertical: PropTypes.boolean
+  vertical: PropTypes.bool,
+  sameTab: PropTypes.bool,
+  hoverOpacity: PropTypes.number,
+  hoverColor: PropTypes.string,
+  margin: PropTypes.objectOf(PropTypes.string),
+  padding: PropTypes.objectOf(PropTypes.string)
 };
 
 ContainerDiv.defaultProps = {
+  icons: [],
   bgColor: 'rgba(0, 0, 0, 0)',
   iconColor: '#FFF',
+  iconOpacity: 1,
   iconSize: '1em',
-  vertical: false
+  vertical: false,
+  sameTab: false,
+  hoverOpacity: 1,
+  hoverColor: '#FFF',
+  margin: {
+    top: '5px',
+    right: '5px',
+    bottom: '5px',
+    left: '5px'
+  },
+  padding: {
+    top: '0',
+    right: '0',
+    bottom: '0',
+    left: '0'
+  }
+};
+
+const IconAnchor = styled.a`
+  text-decoration: none;
+  color: ${({ iconColor }) => iconColor};
+  opacity: ${({ iconOpacity }) => iconOpacity};
+  font-size: ${({ iconSize }) => iconSize};
+  margin-top: ${({ margin }) => margin.top};
+  margin-left: ${({ margin }) => margin.left};
+  margin-right: ${({ margin }) => margin.right};
+  margin-bottom: ${({ margin }) => margin.bottom};
+  padding-top: ${({ padding }) => padding.top};
+  padding-left: ${({ padding }) => padding.left};
+  padding-right: ${({ padding }) => padding.right};
+  padding-bottom: ${({ padding }) => padding.bottom};
+  &:hover {
+    color: ${({ hoverColor }) => hoverColor};
+    opacity: ${({ hoverOpacity }) => hoverOpacity};
+  }
+`;
+
+IconAnchor.propTypes = {
+  href: PropTypes.string.isRequired,
+  target: PropTypes.string.isRequired,
+  iconColor: PropTypes.string,
+  iconOpacity: PropTypes.number,
+  iconSize: PropTypes.string,
+  hoverOpacity: PropTypes.number,
+  hoverColor: PropTypes.string,
+  sameTab: PropTypes.bool,
+  margin: PropTypes.objectOf(PropTypes.string),
+  padding: PropTypes.objectOf(PropTypes.string)
+};
+
+IconAnchor.defaultProps = {
+  link: '/',
+  iconColor: '#FFF',
+  iconOpacity: 1,
+  iconSize: '1em',
+  hoverOpacity: 1,
+  hoverColor: '#FFF',
+  sameTab: false,
+  margin: {
+    top: '5px',
+    right: '5px',
+    bottom: '5px',
+    left: '5px'
+  },
+  padding: {
+    top: '0',
+    right: '0',
+    bottom: '0',
+    left: '0'
+  }
 };
