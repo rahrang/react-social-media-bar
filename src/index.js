d0a17cd4ccecdd3ad8a63c594454bbb8ccdf6c5d
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-11-14 22:47:50 -0800

    initial commit, trying to get everything working

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class SocialMediaBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is so SocialMsdfsaediaBar!</h1>
+      </div>
+    );
+  }
+}
+export default SocialMediaBar;
