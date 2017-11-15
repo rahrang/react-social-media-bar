050c8ff78f8008735916d3c9edab830a68c2134f
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-11-14 23:03:21 -0800

    got basic styled-components figured out, will do more work tomorrow

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-class SocialMediaBar extends React.Component {
+// import { css, StyleSheet } from 'aphrodite';
+import styled from 'styled-components';
+
+export default class SocialMediaBar extends React.Component {
   render() {
     return (
+      // <div className={css(styles.socialMediaBarContainer)}>
       <div>
         <h1>This is so SocialMsdfsaediaBar!</h1>
+        <ContainerDiv>In the container div</ContainerDiv>
       </div>
     );
   }
 }
-export default SocialMediaBar;
+
+// const styles = {
+//   socialMediaBarContainer: {
+//     dislay: 'flex',
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// };
+
+const ContainerDiv = styled.div`
+  background-color: #dddddd;
+  color: #895fad;
+  font-size: 40px;
+`;
